c7c7c56992209ee8ea325b9b521aa75aeda34df3
Daniil Sitnikov <sitnikovdan13@gmail.com>
2026-02-16 23:07:06 +0400

    chore: update tests

--- lib/api/googleBooks/googleBooks.test.ts
+++ lib/api/googleBooks/googleBooks.test.ts
@@ -1,15 +1,13 @@
 import { beforeAll, describe, expect, it } from "vitest";
 
-let getGoogleBooksCover: (
-  imageLinks?: {
-    smallThumbnail?: string;
-    thumbnail?: string;
-    small?: string;
-    medium?: string;
-    large?: string;
-    extraLarge?: string;
-  },
-) => string | undefined;
+let getGoogleBooksCover: (imageLinks?: {
+  smallThumbnail?: string;
+  thumbnail?: string;
+  small?: string;
+  medium?: string;
+  large?: string;
+  extraLarge?: string;
+}) => string | undefined;
 
 describe("getGoogleBooksCover", () => {
   beforeAll(async () => {
@@ -19,14 +17,14 @@ describe("getGoogleBooksCover", () => {
     ({ getGoogleBooksCover } = await import("./index"));
   });
 
-  it("prefers medium over other image sizes", () => {
+  it("prefers thumbnail over other image sizes", () => {
     const cover = getGoogleBooksCover({
       medium: "http://books.google.com/medium.jpg",
       smallThumbnail: "http://books.google.com/small-thumbnail.jpg",
       thumbnail: "http://books.google.com/thumbnail.jpg",
     });
 
-    expect(cover).toBe("https://books.google.com/medium.jpg");
+    expect(cover).toBe("https://books.google.com/thumbnail.jpg");
   });
 
   it("returns undefined when image links are missing", () => {
